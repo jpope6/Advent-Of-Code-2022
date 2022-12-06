bd8168768b8fc17dac60f792b088faa37db47062
jpope6 <imthepope@csu.fullerton.edu>
2022-12-06 08:48:36 -0800

    day 3 complete

---
+++ Day3/part2.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string line;
+    ifstream myfile("input.txt");
+
+    vector<string> first;
+    vector<string> second;
+    vector<string> third;
+
+    int line_index = 1;
+
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            if (line_index == 1) {
+                first.push_back(line);
+                line_index = 2;
+            } else if (line_index == 2) {
+                second.push_back(line);
+                line_index = 3;
+            } else if (line_index == 3) {
+                third.push_back(line);
+                line_index = 1;
+            }
+        }
+    }
+
+    map<char, int> mymap = {
+        {'a', 1},
+        {'b', 2},
+        {'c', 3},
+        {'d', 4},
+        {'e', 5},
+        {'f', 6},
+        {'g', 7},
+        {'h', 8},
+        {'i', 9},
+        {'j', 10},
+        {'k', 11},
+        {'l', 12},
+        {'m', 13},
+        {'n', 14},
+        {'o', 15},
+        {'p', 16},
+        {'q', 17},
+        {'r', 18},
+        {'s', 19},
+        {'t', 20},
+        {'u', 21},
+        {'v', 22},
+        {'w', 23},
+        {'x', 24},
+        {'y', 25},
+        {'z', 26},
+        {'A', 27},
+        {'B', 28},
+        {'C', 29},
+        {'D', 30},
+        {'E', 31},
+        {'F', 32},
+        {'G', 33},
+        {'H', 34},
+        {'I', 35},
+        {'J', 36},
+        {'K', 37},
+        {'L', 38},
+        {'M', 39},
+        {'N', 40},
+        {'O', 41},
+        {'P', 42},
+        {'Q', 43},
+        {'R', 44},
+        {'S', 45},
+        {'T', 46},
+        {'U', 47},
+        {'V', 48},
+        {'W', 49},
+        {'X', 50},
+        {'Y', 51},
+        {'Z', 52},
+    };
+
+    int priority = 0;
+
+    for (int i = 0; i < first.size(); i++) {
+        for (int j = 0; j < first[i].size(); j++) {
+            if (find(second[i].begin(), second[i].end(), first[i][j]) != second[i].end()) {
+                if (find(third[i].begin(), third[i].end(), first[i][j]) != third[i].end()) {
+                    priority += mymap.at(first[i][j]);
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << priority << endl;
+}
