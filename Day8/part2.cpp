1ec80f3111a1e1d9ab49f9971afc762a33831448
jpope6 <imthepope@csu.fullerton.edu>
2022-12-08 08:24:49 -0800

    day 8 complete

--- Day8/part2.cpp
+++ Day8/part2.cpp
@@ -22,51 +22,65 @@ int main() {
     int rows = grid.size();
     int cols = grid[0].size();
 
-    int count = 0;
+    int highest = 0;
 
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
             int left = row;
+            int left_count = 0;
             while (left > 0) {
                 if (grid[left - 1][col] >= grid[row][col]) {
+                    left_count++;
                     break;
                 } else {
+                    left_count++;
                     left--;
                 }
             }
 
             int right = row;
+            int right_count = 0;
             while (right < rows - 1) {
                 if (grid[right + 1][col] >= grid[row][col]) {
+                    right_count++;
                     break;
                 } else {
+                    right_count++;
                     right++;
                 }
             }
 
             int up = col;
+            int up_count = 0;
             while (up > 0) {
                 if (grid[row][up - 1] >= grid[row][col]) {
+                    up_count++;
                     break;
                 } else {
+                    up_count++;
                     up--;
                 }
             }
 
             int down = col;
+            int down_count = 0;
             while (down < cols - 1) {
                 if (grid[row][down + 1] >= grid[row][col]) {
+                    down_count++;
                     break;
                 } else {
+                    down_count++;
                     down++;
                 }
             }
 
-            if (left == 0 || right == rows - 1 || up == 0 || down == cols - 1) {
-                count++;
+            int score = left_count * right_count * up_count * down_count;
+
+            if (score > highest) {
+                highest = score;
             }
         }
     }
 
-    cout << count << endl;
+    cout << highest << endl;
 }
