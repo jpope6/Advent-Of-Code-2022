e545b5366789b511280c77a7adc34ba49d4c03ca
jpope6 <imthepope@csu.fullerton.edu>
2022-12-11 14:32:21 -0800

    day 10 part 2 not working right

--- Day10/part2.cpp
+++ Day10/part2.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+void drawSquare(int position[3], int& index, int& count, vector<string>& s) {
+    int row = index % 6;
+    int col = index % 40;
+
+    cout << row << ", " << col << endl;
+
+    for (int i = 0; i < 3; i++) {
+        if (col == position[i]) {
+            s[row][col] = '#';
+        }
+    }
+}
+
 int main() {
     string line;
     ifstream myfile("input.txt");
@@ -15,33 +28,60 @@ int main() {
     }
 
     string s = "";
+    vector<string> grid;
 
-    for (int i = 0; i < 240; i++) {
+    for (int i = 1; i <= 240; i++) {
         s = s + '.';
+
+        if (i % 40 == 0) {
+            grid.push_back(s);
+            s = "";
+        }
     }
 
-    int cycle = 1;
-    int count = 1;
-    vector<int> signal_strength;
+    grid.push_back(s);
 
-    int position[3] = {1, 2, 3};
-    bool isVisible = false;
+    int cycle = 1;
+    int X = 1;
+
+    int index = 0;
+
+    int position[3] = {0, 1, 2};
 
     for (int i = 0; i < directions.size(); i++) {
+        drawSquare(position, index, X, grid);
         if (directions[i] == "noop") {
-            int x = 0;
-        }
-        else
-        {
+            index++;
+        } else {
             int num = stoi(directions[i].substr(5, directions[i].length() - 1));
+            index++;
 
-            count = count + num;
+            // cout << num << endl;
 
-            position[0] = count - 1;
-            position[1] = count;
-            position[2] = count + 1;
+            drawSquare(position, index, X, grid);
+            X = X + num;
+            index++;
 
-            
+            // cout << X << endl;
+
+            position[0] = X - 1;
+            position[1] = X;
+            position[2] = X + 1;
+
+            // cout << position[0] << ", " << position[1] << ", " << position[2] << endl;
         }
+    }
+
+    for (string s : grid) {
+        for (char c : s) {
+            if (c == '.') {
+                // black square
+                cout << "\U00002B1B";
+            } else {
+                // white square
+                cout << "\U00002B1C";
+            }
+        }
+        cout << endl;
     }
 }
