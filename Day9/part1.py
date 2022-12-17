a961d439566c0689fbdd75940d77be296700f4ee
jpope6 <imthepope@csu.fullerton.edu>
2022-12-17 09:57:22 -0800

    day 9 part 1 complete

---
+++ Day9/part1.py
@@ -0,0 +1,4 @@
+with open("Day9/input.txt") as file:
+    lines = [line.rstrip() for line in file.readlines()]
+
+
---
+++ Day9/part2.py
@@ -0,0 +1,55 @@
+with open("Day9/input.txt") as file:
+    lines = [line.rstrip() for line in file.readlines()]
+
+visited = set()
+head = [0, 0]
+tail = [0, 0]
+index = 0
+
+for line in lines:
+    line = line.split()
+
+    index = int(line[1])
+
+    for i in range(index):
+
+        if line[0] == "R":
+            head[0] += 1
+
+        if line[0] == "L":
+            head[0] -= 1
+
+        if line[0] == "U":
+            head[1] += 1
+
+        if line[0] == "D":
+            head[1] -= 1
+
+        if head[0] - tail[0] == 2:
+            if head[1] != tail[1]:
+                tail[1] += head[1] - tail[1]
+
+            tail[0] += 1
+
+        if head[0] - tail[0] == -2:
+            if head[1] != tail[1]:
+                tail[1] += head[1] - tail[1]
+
+            tail[0] -= 1
+
+        if head[1] - tail[1] == 2:
+            if head[0] != tail[0]:
+                tail[0] += head[0] - tail[0]
+
+            tail[1] += 1
+
+        if head[1] - tail[1] == -2:
+            if head[0] != tail[0]:
+                tail[0] += head[0] - tail[0]
+
+            tail[1] -= 1
+
+        visited.add((tail[0], tail[1]))
+
+
+print(len(visited))
